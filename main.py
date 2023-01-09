1713a1f4137fb7e065b86a49e62dcd72009be6cc
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-09 17:42:37 -0500

    adds next question

--- main.py
+++ main.py
@@ -11,4 +11,6 @@ for question in question_data:
     questions.append(new_question)
 
 quiz = QuizBrain(questions)
-quiz.next_question()
+
+while quiz.questions_remaining():
+    quiz.next_question()
--- quiz_brain.py
+++ quiz_brain.py
@@ -3,8 +3,25 @@ class QuizBrain:
     def __init__(self, questions):
         self.questions_list = questions
         self.question_number = 0
+        self.score = 0
+
+    def questions_remaining(self):
+        return self.question_number < len(self.questions_list)
 
     def next_question(self):
         current_question = self.questions_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text}\n")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text}\n")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, answer):
+        if user_answer.lower() == answer.lower():
+            print("Correct!")
+            self.score += 1
+        else:
+            print("Wrong!")
+        print(f"{answer} is the answer.")
+        print(f"Score: {self.score} out of {self.question_number}")
+
+
+   
