45b943c2779ac79277aeed12a9670f4210ed5830
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-09 11:13:22 -0500

    Adds somu bird questions

---
+++ data.py
@@ -0,0 +1,34 @@
+bird_questions = [
+    {
+        "text": "Which flightless bird is also the tallest bird in South America?",
+        "answer": "Greater Rhea",
+    },
+    {
+        "text": " Where is the single largest colony of Puffins situated in the world?",
+        "answer": "Westman Isles",
+    },
+    {
+        "text": "Which bird's male counterpart has a red colored gular sac near its throat and chest area?",
+        "answer": "Frigatebird",
+    },
+    {
+        "text": "How many states have the Red Cardinal as their official bird in North America?",
+        "answer": "Seven",
+    },
+    {
+        "text": "Which bird has the longest bill in the world, measuring half a meter in length?",
+        "answer": "Australian Pelican",
+    },
+    {
+        "text": "Which country has the Raven as its national bird?",
+        "answer": "Bhutan",
+    },
+    {
+        "text": "Which owl-faced bird is native only to New Zealand?",
+        "answer": "Kakapo",
+    },
+    {
+        "text": "What is the largest flying bird native to Africa?",
+        "answer": "Ardeotis",
+    },
+]
